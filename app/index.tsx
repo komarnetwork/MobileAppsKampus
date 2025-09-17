9025920788ced6aa35ff61f2669236cff5a2ad1e
komarnetwork <komar.network@gmail.com>
2025-09-17 21:06:00 +0700

    update ios

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { Text, View, Image, ScrollView, TextInput, Button, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { Text, Image, TextInput, Pressable } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
-  const [alas, setAlas] = useState('')
-  const [tinggi, setTinggi] = useState('')
-  const [message, setMessage] = useState('')
+  const [alas, setAlas] = useState('');
+  const [tinggi, setTinggi] = useState('');
+  const [message, setMessage] = useState('');
 
   const hitungLuas = () => {
     const alasNum = parseFloat(alas);
@@ -17,11 +18,11 @@ export default function Index() {
 
     const hasil = (alasNum * tinggiNum) / 2;
     setMessage(`Hasilnya adalah: ${hasil} satuan luas`);
-  }
+  };
 
   return (
-    <ScrollView>
-      <View className='flex-1 items-center justify-center px-5'>
+    <SafeAreaProvider>
+      <SafeAreaView className='flex-1 items-center justify-center px-4'>
         <Text className='text-3xl font-bold text-center underline text-blue-500 p-5'>
           Tugas 3 - Hitung Luas Segitiga
         </Text>
@@ -31,42 +32,46 @@ export default function Index() {
           source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
         />
 
-        <Text className='text-center text-2xl p-5'>
+        <Text className='text-center text-2xl mt-5'>
           Halo, selamat datang di aplikasi React Native menggunakan Expo!
         </Text>
 
         <TextInput
-          className='border border-blue-600 rounded-xl my-2 w-full px-5'
+          className='border border-blue-600 rounded-lg my-2 w-full ios:h-12 px-5 android:border-[1.5px] placeholder:text-gray-500'
           placeholder='Masukkan Alas'
+          placeholderTextColor='#6B7280'
           value={alas}
           onChangeText={setAlas}
           keyboardType='numeric'
+          underlineColorAndroid='transparent'
         />
         <TextInput
-          className='border border-blue-600 rounded-xl my-2 w-full px-5'
+          className='border border-blue-600 rounded-lg my-2 w-full ios:h-12 px-5 android:border-[1.5px] placeholder:text-gray-500'
           placeholder='Masukkan Tinggi'
+          placeholderTextColor='#6B7280'
           value={tinggi}
           onChangeText={setTinggi}
           keyboardType='numeric'
+          underlineColorAndroid='transparent'
         />
 
-        <TouchableOpacity
-          className='bg-blue-500 py-3 px-6 my-2 w-full rounded-lg items-center justify-center'
+        <Pressable
+          className='bg-blue-500 rounded-xl py-3 px-10 mt-4'
           onPress={hitungLuas}
         >
-          <Text className='text-white text-base font-bold'>
+          <Text className='text-white text-center font-bold'>
             Tampilkan Hasil
           </Text>
-        </TouchableOpacity>
+        </Pressable>
 
         {message ? (
           <Text
-            className={`font-bold text-2xl p-5 ${message.includes('valid') ? 'text-red-600' : 'text-green-600'}`}
+            className={`font-bold text-2xl mt-5 ${message.includes('valid') ? 'text-red-600' : 'text-green-600'} shadow-none elevation-none`}
           >
             {message}
           </Text>
         ) : null}
-      </View>
-    </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
